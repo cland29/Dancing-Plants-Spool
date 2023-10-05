0a73aec3e766ed4304951ae9319e06764db0116b
cland29 <carl.lee.landskron@gmail.com>
2023-10-05 12:14:58 -0400

    Add server and address

--- Python/Dancing_Plants/Main.py
+++ Python/Dancing_Plants/Main.py
@@ -1,7 +1,26 @@
-
+import socket
+import threading
 
 
 def main():
+    print("hello world")
+
+    PORT = 5050
+    SERVER = socket.gethostbyname(socket.gethostname())
+    ADDR = (SERVER, PORT)
+    print(SERVER)
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+
+    start_server()
+
+def handle_client(conn, addr):
+    pass
+
+def start_server():
+    pass
+
 
 
 
