a8e78af5ea6d3488fa9f4c214ca3c81513e4a24f
cland29 <carl.lee.landskron@gmail.com>
2023-10-05 15:39:17 -0400

    Add server to receive messages from client

--- Python/Dancing_Plants/Main.py
+++ Python/Dancing_Plants/Main.py
@@ -1,25 +1,48 @@
 import socket
 import threading
 
-
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 def main():
     print("hello world")
 
-    PORT = 5050
-    SERVER = socket.gethostbyname(socket.gethostname())
-    ADDR = (SERVER, PORT)
+
     print(SERVER)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
 
-    start_server()
+    print("[Starting] Server is starting")
+    start_server(server)
 
 def handle_client(conn, addr):
-    pass
+    print(f"[NEW CONNECTION] {addr} connected")
 
-def start_server():
-    pass
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(f"[{addr}] {msg}")
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+    conn.close()
+
+
+def start_server(server: socket):
+    server.listen()
+    print(f"[LISTENING] SERVER is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
 
 
 
