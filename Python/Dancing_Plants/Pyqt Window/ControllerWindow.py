ace6e59e3f318b579039b21463179287f88a5cc5
cland29 <carl.lee.landskron@gmail.com>
2023-11-05 00:10:55 -0400

    Rework architecture - This commit focused on reworking the code so it allowed for the button/slider to be live and change

--- Python/Dancing_Plants/Pyqt Window/ControllerWindow.py
+++ Python/Dancing_Plants/Pyqt Window/ControllerWindow.py
@@ -5,15 +5,15 @@ from PyQt6.QtCore import *
 import time
 
 class Position_Selector(QWidget):
-    def __init__(self, min_pos, max_pos, step_size, pos_update_func):
+    def __init__(self, min_pos, max_pos, step_size):
         super(Position_Selector, self).__init__()
 
-        self.cur_pos = 0
+
         self.min_pos = min_pos
         self.max_pos = max_pos
         self.step_size = step_size
         self.initUI()
-        self.pos_update_func = pos_update_func
+
 
     def initUI(self):
         vert_layout = QVBoxLayout()
@@ -25,40 +25,30 @@ class Position_Selector(QWidget):
         self.value_widget.setSingleStep(self.step_size)
         self.value_widget.valueChanged.connect(self.change_pos)
 
-        label = QLabel("Check for Live Update:")
 
-        self.live_update_widget = QCheckBox()
-        self.live_update_widget.setCheckState(Qt.CheckState.Unchecked)
-        self.live_update_widget.stateChanged.connect(self.set_cur_pos)
-
-        hor_layout.addWidget(self.value_widget)
-        hor_layout.addWidget(label)
-        hor_layout.addWidget(self.live_update_widget)
 
         self.slider_widget = QSlider(Qt.Orientation.Horizontal)
         self.slider_widget.setMinimum(self.min_pos)
         self.slider_widget.setMaximum(self.max_pos)
         self.slider_widget.valueChanged.connect(self.change_pos)
 
-        vert_layout.addLayout(hor_layout)
+        vert_layout.addWidget(self.value_widget)
         vert_layout.addWidget(self.slider_widget)
 
-        #full_widget = QWidget()
+
         self.setLayout(vert_layout)
 
     def change_pos(self, new_pos):
-        self.cur_pos = new_pos
-        self.slider_widget.setValue(self.cur_pos)
-        self.value_widget.setValue(self.cur_pos)
+        self.slider_widget.setValue(new_pos)
+        self.value_widget.setValue(new_pos)
 
-        self.set_cur_pos()
+    def get_pos(self):
+        return self.value_widget.value()
 
-    def set_cur_pos(self):
-        if (self.is_pos_live()):
-            self.pos_update_func(self.cur_pos)
 
-    def is_pos_live(self) -> bool:
-        return self.live_update_widget.checkState() == Qt.CheckState.Checked
+
+
+
 
 
 
@@ -67,30 +57,25 @@ class Position_Selector(QWidget):
 class ControllerWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(ControllerWindow, self).__init__(*args, **kwargs)
-        self.pos_list = []
+        self.pos_sel_widgets = []
         self.initUI()
 
     def initUI(self):
             self.setWindowTitle("my window")
             self.num = 2
 
+            top_layout = QHBoxLayout()
+
+            for i in range(3):
+                self.pos_sel_widgets.append(Position_Selector(0, 100, 2))
+                top_layout.addWidget(self.pos_sel_widgets[i])
+
+
+
 
             self.pos_list = QListWidget()
-            self.pos_list.addItems(["one", "1,3,4", "3,5,6"])
+            self.pos_list.addItems([])
             self.pos_list.currentItemChanged.connect(self.set_position_values)
-
-
-
-
-            # creating a button to be clicked
-            #button1 = QPushButton('Button-1', self)
-            #        button1.move(100, 70)
-            #button1.clicked.connect(self.on_click)
-
-            top_layout = QHBoxLayout()
-            top_layout.addWidget(Position_Selector(0, 100, 2, self.set_pos1))
-            top_layout.addWidget(Position_Selector(0, 100, 2, self.set_pos2))
-            top_layout.addWidget(Position_Selector(0, 100, 2, self.set_pos3))
 
 
             self.layout = QVBoxLayout(self)
@@ -113,40 +98,25 @@ class ControllerWindow(QMainWindow):
 
             self.setCentralWidget(widget)
 
-    @pyqtSlot()
-    def on_click(self):
-            print('Button-{} will be created'.format(self.num))
-            button2 = QPushButton('Button-{}'.format(self.num), self)
-            button2.clicked.connect(lambda: print(button2.text()))
-
-            button2.move(0, 30 * (self.num - 1))
-            button2.show()
-
-            self.layout.addWidget(button2)
-            self.num += 1
 
     def set_position_values(self, passed_val):
         print(passed_val.text())
         setpoint_val = passed_val.text().split(",")
-        if(len(setpoint_val) == 3 and setpoint_val[0].isnumeric() and setpoint_val[1].isnumeric() and setpoint_val[2].isnumeric()):
-            self.pos1 = int(setpoint_val[0])
-            self.pos2 = int(setpoint_val[1])
-            self.pos3 = int(setpoint_val[2])
-        else:
-            print("Not numeric Inputs of list 3")
-        print("Positions changed to:",self.pos1, self.pos2, self.pos3)
+        print(setpoint_val)
 
-    def set_pos1(self, update_pos):
-        self.pos1 = update_pos
+        for i in range(len(self.pos_sel_widgets)):
+            self.pos_sel_widgets[i].change_pos(int(setpoint_val[i]))
 
-    def set_pos2(self, update_pos):
-        self.pos2 = update_pos
 
-    def set_pos3(self, update_pos):
-        self.pos3 = update_pos
 
     def sav_full_pos(self):
-        self.pos_list.addItem(f"{self.pos1},{self.pos2},{self.pos3}")
+        #Did it this way so that you would have comma's in between values but not at the very end
+        pos = ""
+        for i in range(len(self.pos_sel_widgets) - 1):
+            pos = pos + str(self.pos_sel_widgets[i].get_pos()) + ","
+        pos = pos + str(self.pos_sel_widgets[-1].get_pos())
+
+        self.pos_list.addItem(pos)
 
 
 
