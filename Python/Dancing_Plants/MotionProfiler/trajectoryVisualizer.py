86a57ac5cbbd67f117b8a83c578a599af8dae793
cland29 <carl.lee.landskron@gmail.com>
2023-11-08 12:00:52 -0500

    Add camera perspective and remove shader code

--- Python/Dancing_Plants/MotionProfiler/trajectoryVisualizer.py
+++ Python/Dancing_Plants/MotionProfiler/trajectoryVisualizer.py
@@ -3,14 +3,27 @@ import ctypes
 
 import numpy as np
 from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
 
 from OpenGL.GL.shaders import compileProgram, compileShader
 
 from PyQt6.QtGui import QSurfaceFormat
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtOpenGL import QOpenGLWindow, QOpenGLVersionProfile
+#from PyQt6.QtOpenGL import QOpenGLWindow, QOpenGLVersionProfile
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
 
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtOpenGL import *
+
+
+from functools import partial
+
+import pandas as pd
 
 ## Initial pyOpengl set up from this tutorial: https://www.youtube.com/watch?v=lFFYmP528Mw
 
@@ -41,7 +54,74 @@ void main()
    color = vec4(fragmentColor, 1.0);
 }
 '''
+
+
+
+added_vertices = pd.read_csv("data.csv", header = None)
+
+added_vertices = np.array(added_vertices)
+
+rot_angle = 0
+
+def Path():
+    glBegin(GL_LINE_STRIP)
+    for i in added_vertices:
+        glVertex3fv(i[:3])
+    glEnd()
+
+
+print(added_vertices)
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(QMainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Trajectory Visualizer Demo")
+
+        point_selectors_layout = QVBoxLayout()
+        self.widget_list = []
+        for i in range(6):
+            point_select_layout = QHBoxLayout()
+
+            name_widget = QLabel(f"Point {i}")
+            point_select_layout.addWidget(name_widget)
+
+            for n in range(3):
+                widget = QSpinBox()
+                widget.setMinimum(-10)
+                widget.setMaximum(10)
+                widget.setSingleStep(1)
+                widget.setValue(added_vertices[i][n])
+                widget.valueChanged.connect(partial(self.change_vector_array, len(self.widget_list), i, n))
+                self.widget_list.append(widget)
+
+                point_select_layout.addWidget(widget)
+            point_selectors_layout.addLayout(point_select_layout)
+
+
+
+        save_button = QPushButton()
+        save_button.pressed.connect(self.save_points)
+        point_selectors_layout.addWidget(save_button)
+
+        widget = QWidget()
+        widget.setLayout(point_selectors_layout)
+        self.setCentralWidget(widget)
+
+    def change_vector_array(self, val, x, y):
+        added_vertices[x][y] = self.widget_list[val].value()
+        print(val,x, y, self.widget_list[val].value())
+
+    def save_points(self):
+        df = pd.DataFrame(added_vertices)
+        df.to_csv('data.csv', index=False, header=False)
+
+    def angle_changed(self, angle):
+        rot_angle = angle
+
+
 class GLWindow(QOpenGLWindow):
+
+
+
     def initializeGL(self):
         self.fmt = QOpenGLVersionProfile()
         self.fmt.setVersion(3, 3)
@@ -49,42 +129,32 @@ class GLWindow(QOpenGLWindow):
 
         print(f"Running {glGetString(GL_VERSION)}")
 
-        self.shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
-                                     compileShader(fragment_src, GL_FRAGMENT_SHADER))
+        gluPerspective(45, (1), 0.1, 50.0)
 
-        glUseProgram(self.shader)
+        glTranslatef(0.0, 0.0, -15)
 
-        self.vertices = (
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-            0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-            0.0, 0.5, 0.0, 0.0, 0.0, 1.0
-        )
+        self.count = 0
 
-        self.vertices = np.array(self.vertices, dtype=np.float32)
-
-        self.vbo = glGenBuffers(1)
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+        self.timer = QTimer(self)
+        self.timer.setInterval(20)
+        self.timer.timeout.connect(self.update)
+        self.timer.start()
 
     def paintGL(self):
-        glClear(GL_COLOR_BUFFER_BIT)
-        glUseProgram(self.shader)
-        glBindVertexArray(self.vao)
-        glDrawArrays(GL_TRIANGLES, 0, 6)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # Cube()
+        Path()
 
 app = QApplication(sys.argv)
-window = GLWindow()
-window.resize(QSize(800, 600))
+window = MainWindow()
+window2 = GLWindow()
+window.resize(QSize(400, 600))
+window2.resize(QSize(800, 600))
 window.show()
+window2.show()
 app.exec()
+
+
+
 
 
